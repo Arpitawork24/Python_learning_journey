0ec5d0b2d2e1c14a47ba484800cd940c673d7836
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-04 00:54:10 +0530

    greeting_exercise

---
+++ exercises/logic_questions/greeting.py
@@ -0,0 +1,17 @@
+import time
+
+#get current time components
+hour = int(time.strftime('%H'))
+min = int(time.strftime('%M'))
+sec = int(time.strftime('%s'))
+
+print("current time:", time.strftime('%H:%M:%S'))
+if (hour > 5) or (hour == 5 and min >= 0):
+    if hour < 12:
+        print("Good Morning!")
+    elif hour < 17:
+        print("Good Afternoon!")
+    else:
+        print("Good Evening!")
+else:
+    print("Good Evening!")
