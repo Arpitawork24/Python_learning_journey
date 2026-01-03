3dfcaa72737d5fd7379f754ae8bd0d882e8ffe09
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-04 00:03:16 +0530

    Create palindrome.py

---
+++ exercises/logic_questions/palindrome.py
@@ -0,0 +1,13 @@
+# Check palindrome number
+num = int(input("Enter number: "))
+temp = num
+rev = 0
+
+while num > 0:
+    rev = rev * 10 + num % 10
+    num //= 10
+
+if temp == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
