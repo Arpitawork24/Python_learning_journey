490ddd17a31ecff2e9b49c6a4242987780d58ca4
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-10 00:13:04 +0530

    dict basics

--- notes/dictionary.py
+++ notes/dictionary.py
@@ -3,13 +3,14 @@
 roll = {
     1 : "Arpita",
     2: "Piyu",
-    3: "Raj"
+    3: "Raj",
+    4 : "cutiee"
 }
 
 print(roll) #printing all key with their values
-print(roll.values()) #this will print all values only without key
-print(roll.keys()) #this will print all keys only without values
-print(roll.items()) #this will print all keys nd values pairs
+print("only values-",roll.values()) #this will print all values only without key
+print("only keys-",roll.keys()) #this will print all keys only without values
+print("all key-values-\n",roll.items()) #this will print all keys nd values pairs
 print(roll[2]) #accessing value roll no 2 as 2 is key
 # print(roll["Piyu"]) #this will give error bcz piyu is key nd in dict only values can be accessed using key
 print(roll.get(1)) #can also be accessed using get method
@@ -18,16 +19,22 @@ print(roll.get(1)) #can also be accessed using get method
 (roll.update({3: "Rahul" , 1: "Sanu"})) #updating 2 values
 print(roll) #updated
 
+roll.pop(3)
+print(roll) #popped key-value pair whose key is mentioned i.e. 3
+roll.popitem()
+print(roll) #pops laust key-value pair
+roll.clear()
+print(roll) #dict cleared
 
 #eg-2
 
-Result = {
+result = {
     "Arpita" : 80,
     "Rohan" : 54,
     "Piyu" : 85
     
 }
 
-print("Result",Result) #accessing all result
-print(Result["Rohan"]) #accessing rohan's result as rohan is key
-print(Result.get("Arpita"))
+print("Result-\n",result) #accessing all result
+print(result["Rohan"]) #accessing rohan's result as rohan is key
+print(result.get("Arpita"))
