62dbf28e5791e0034f658afce272b76d04f80b1f
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-10 00:53:15 +0530

    for with else done

--- notes/special for loop/for_with_else.py
+++ notes/special for loop/for_with_else.py
@@ -3,11 +3,11 @@
 for i in range(6):
     print(i)
 else:
-    print("no i or i is over") #after for loop is completed this will run.
+    print("no i or i is over\n") #after for loop is completed this will run.
 
 for i in range(6):
     print(i)
     if i==3 :
         break;
 else:
-    print("no i or i is over") #after for loop is completed this will run.
+    print("no i or i is over") #this else loop wont run bcz for loop was broken or not completed fully
