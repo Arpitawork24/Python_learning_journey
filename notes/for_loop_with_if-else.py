bfa24998d69072525eed1d9ae4f54a33790c40cb
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-10 00:32:45 +0530

    for with if

---
+++ notes/for_loop_with_if-else.py
@@ -0,0 +1,26 @@
+words = {"hii", "Arpita", "bird", "bad", "nice"}
+
+for item in words :
+    if item == "bad":
+        break;
+    print(item) #this will print unordered as words is a kind of set
+    
+for i in range(6):
+    if i >3 :
+        break;
+    print(i)
+
+print("----------")
+
+for i in range(6):
+    if i == 3 :
+        break;
+    print(i)
+
+print("----------")
+
+for i in range(6):
+    if i == 3 :
+        continue;
+    print(i)
+
