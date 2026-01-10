39477d53c0ffea2c0f40948c90957696657a6187
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-10 23:45:48 +0530

    edit while

--- notes/special for loop/while_with_else.py
+++ notes/special for loop/while_with_else.py
@@ -8,7 +8,7 @@ while i < 7:
 else :
     print("while loop is over \n") #after for loop is completed this will run.
 
-
+#useless else with while loop
 i = 0
 while i < 7:
     print(i)
@@ -16,4 +16,17 @@ while i < 7:
     if i==4:
         break
 else :
-    print("while loop is over") #this else loop wont run bcz for loop was broken or not completed fully
+    print("while loop is over\n \n") #this else loop wont run bcz for loop was broken or not completed fully
+
+
+#useful else with while loop
+print("last example \n")
+
+i = 0
+while i < 7:
+    
+    i=i+1
+    if i==9:
+        break
+else :
+    print("while loop is over")
