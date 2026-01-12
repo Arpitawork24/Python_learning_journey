cff3e30b48146a6febd053088c1005b0abba6328
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-13 00:49:49 +0530

    facto

--- exercises/logic_questions/factorial.py
+++ exercises/logic_questions/factorial.py
@@ -1,9 +1,12 @@
 # a type of recursive function
 
 def factorial(n):
-    if n == 0:
+    if n == 0:  # Base case
         return 1
-    else:
+    else:        # Recursive case
         return n * factorial(n - 1)
 
 print(factorial(4))
+
+# Base Case: When n == 0, recursion stops and returns 1.
+# Recursive Case: Multiplies n with the factorial of n-1 until it reaches the base case.
