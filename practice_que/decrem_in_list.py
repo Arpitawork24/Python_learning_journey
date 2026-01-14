b8a78a4b0b3a0d03679ef2b06a37379d1f644357
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-14 23:46:04 +0530

    decre in list

---
+++ practice_que/decrem_in_list.py
@@ -0,0 +1,8 @@
+# You are given a list that contains integers. You need to decrement each element of the list by 1 and return the list.
+
+def decre(arr):
+    for i in range(len(arr)):
+        arr[i]=arr[i]-1
+    return (arr)
+
+print(decre([5,3,7,2]))
