860488e7e989116cb50b73da0716bd0c9347f95b
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-15 23:36:46 +0530

    dict que

---
+++ notes/dictionary/dict_que.py
@@ -0,0 +1,27 @@
+# Taking input and initializing dictionary
+keys = input().split()
+values = map(int, input().split())
+my_dict = dict(zip(keys, values))
+k, v = input().split()
+
+# Insert k,v in my_dict
+my_dict[k]=int(v)
+# Print Inserted if inserted successfuly
+print("Inserted")
+
+d = input()
+if d in my_dict:
+    del my_dict[d]
+    print ("Deleted")
+else :
+    print (-1)
+# Delete entry with key d from my_dict
+# Print Deleted if deleted successfuly else print -1
+
+p = input()
+if p in my_dict:
+    print(f"Marks of {p} is {my_dict[p]}")
+else :
+    print(-1)
+
+# Print marks of given key p if key present, else print -1
