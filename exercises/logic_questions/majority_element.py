e79b8d58295f17a26fc5e709bdf597916599e98a
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-16 00:17:06 +0530

    majority elem

---
+++ exercises/logic_questions/majority_element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, arr):
+        #code here
+        n = len(arr)
+        freq={}
+        for i in range(n):
+            if arr[i] in freq :
+                freq[arr[i]]+=1
+            else:
+                freq[arr[i]] = 1
+        if freq[arr[i]]>n//2:
+            return arr[i]
+        return -1
+
+# correct way to call
+obj = Solution()
+print(obj.majorityElement([2,3,2,2,6,2]))
