7ed7f19cc6611e454ab590252163cddbc609123f
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-15 21:53:15 +0530

    corrected

--- practice_que/decrem_in_list.py
+++ practice_que/decrem_in_list.py
@@ -1,7 +1,7 @@
 # You are given a list that contains integers. You need to decrement each element of the list by 1 and return the list.
 
 def decre(arr):
-    for i in range(len(arr)):  #this is used when - you need to modify the list , you need to modify the list
+    for i in range(len(arr)):  #this is used when - you need to modify the list , you need indices.
         arr[i]=arr[i]-1
     return (arr)
 
--- practice_que/double_tuple.py
+++ practice_que/double_tuple.py
@@ -1,7 +1,7 @@
 def doubleTup(numbers):
     #code here
     temp = list(numbers)
-    for i in range(len(temp)):
+    for i in range(len(temp)):  #this is used when - you need to modify the list , you need indices.
         temp[i]=temp[i]*2
     numbers = tuple(temp)
     return(numbers)
