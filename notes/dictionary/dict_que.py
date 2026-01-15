adc1b8ba7f7f45afc1f67f93fc19a486a5916c2a
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-15 23:37:54 +0530

    dict edit

--- notes/dictionary/dict_que.py
+++ notes/dictionary/dict_que.py
@@ -9,19 +9,18 @@ my_dict[k]=int(v)
 # Print Inserted if inserted successfuly
 print("Inserted")
 
+# Delete entry with key d from my_dict
+# Print Deleted if deleted successfuly else print -1
 d = input()
 if d in my_dict:
     del my_dict[d]
     print ("Deleted")
 else :
     print (-1)
-# Delete entry with key d from my_dict
-# Print Deleted if deleted successfuly else print -1
 
+# Print marks of given key p if key present, else print -1
 p = input()
 if p in my_dict:
     print(f"Marks of {p} is {my_dict[p]}")
 else :
     print(-1)
-
-# Print marks of given key p if key present, else print -1
