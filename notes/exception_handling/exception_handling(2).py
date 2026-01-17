9ee572b364d8af631852a5d84e17f19064c832d3
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-17 12:16:21 +0530

    exception(2)

---
+++ notes/exception_handling/exception_handling(2).py
@@ -0,0 +1,8 @@
+try :
+    num = int(input("enter num = "))
+    a = [6,8,4]
+    print(a[num])
+except ValueError:  #if num = kgf (not integer)
+    print("value error")
+except IndexError:  #if num = 5 (index that doesnt exist rn)
+    print("index error")
