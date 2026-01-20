f778c060cd2dec012ca33cf07b3469e55051a7f5
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-20 12:32:16 +0530

    Enhance sets_basics.py with set operation examples
    Added examples of set operations including remove, discard, and pop.

--- notes/data_structures/sets/sets_basics.py
+++ notes/data_structures/sets/sets_basics.py
@@ -1,9 +1,24 @@
 # Sets are unordered collection of data items nd do not contain duplicate items.
 info = {"Carla", 19, False, 5.9, 19} #here 19 will print only once
 print(info)
+for item in info:
+    print (item)
 
+#TypeError: set.remove() takes exactly one argument , if the element not found then remove() will raise error
+info.remove("Carla")
+print(info)
+
+#discard() method also removes a specified element from the set. Unlike remove(), if the element is not found, it does not raise an error.
+info.discard(5.9)
+print(info)
+
+print("info 1- ")
 #they cannot be accessed using index numbers.
 #accessing using for loop
 info1 = {"Carla", 19, False, 5.9}
 for item in info1:
     print(item)
+
+val = info1.pop()
+print(val)
+print(info1)
