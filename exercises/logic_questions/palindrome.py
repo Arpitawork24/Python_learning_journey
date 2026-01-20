6f4bba7da57dbc2b31449313321a6d8167397e15
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-20 11:39:13 +0530

    Enhance comments in palindrome check program
    Updated comments for clarity and added a description.

--- exercises/logic_questions/palindrome.py
+++ exercises/logic_questions/palindrome.py
@@ -1,12 +1,26 @@
-# Check palindrome number
+# Program to check whether a number is a palindrome
+
+# Take input from the user
 num = int(input("Enter number: "))
+
+# Store the original number in a temporary variable
 temp = num
+
+# Variable to store the reversed number
 rev = 0
 
+# Loop to reverse the number
 while num > 0:
-    rev = rev * 10 + num % 10
+    # Get the last digit of the number
+    digit = num % 10
+
+    # Add the digit to the reversed number
+    rev = rev * 10 + digit
+
+    # Remove the last digit from the number
     num //= 10
 
+# Check if the original number is equal to the reversed number
 if temp == rev:
     print("Palindrome")
 else:
