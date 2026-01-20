a0a0f1b7a3eae6df44d7bb9a520da9f5698cca1c
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-21 00:33:43 +0530

    ifelse

--- notes/conditions/shorthand_if_else.py
+++ notes/conditions/shorthand_if_else.py
@@ -1,3 +1,8 @@
 a = 360
-b = 200
+b = 20000
 print("a") if a>b else print("=") if a == b else print("b")
+print ("good") if a<b else ""
+print ("bad") if a>b else "" # it wont print anything if the else statement is true here or its exectued
+
+c = 9 if a<b else 0
+print(c)
