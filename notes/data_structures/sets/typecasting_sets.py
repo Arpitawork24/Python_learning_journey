c50a8976238d1554c200179f06c5b326bc56e7cb
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-20 12:45:01 +0530

    Add typecasting for sets
    Demonstrate typecasting of list, string, and dictionary into sets.

---
+++ notes/data_structures/sets/typecasting_sets.py
@@ -0,0 +1,14 @@
+# Typecasting list into set
+li = [1, 2, 3, 3, 4, 5, 5, 6, 2]
+set1 = set(li)
+print(set1)
+
+# Typecasting string into set
+s = "GeeksforGeeks"
+set1 = set(s)
+print(set1)
+
+# Typecasting dictionary into set
+d = {1: "One", 2: "Two", 3: "Three"}
+set1 = set(d)
+print(set1)
