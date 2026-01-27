7c9fe7e47c85000dc10623a7d7f6db754410740b
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-28 00:44:59 +0530

    array basics-2

--- notes/array/array_basics.py
+++ notes/array/array_basics.py
@@ -1,6 +1,26 @@
 import array as arr
-a = arr.array("i",[1,2,3])
+
+#i = integer only
+a = arr.array("i",[1,3,4,2,10,3,6])
+
+#accessing element through index
 print(a[0])
 
+#appending 
 a.append(5)
+print(a)
+
+for i in range(3):
+    print(a[i], end=" ")
+
+#adding elements to array
+a.insert(7,8)
+print(a)
+
+# removing first occurence of element (not index)
+a.remove(3)
+print(a)
+
+#removing element at index 3
+a.pop(3)
 print(a)
