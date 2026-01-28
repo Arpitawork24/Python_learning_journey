a2671d81e20698d56f12416e9263fb81bdcd94b7
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-28 13:19:13 +0530

    lambda

---
+++ notes/functions/lambda.py
@@ -0,0 +1,10 @@
+double = lambda x : x*2
+cube = lambda x : x**3
+square = lambda x : x**2
+add = lambda x,y : x+y
+avg = lambda x,y,z : (x+y+z)/3
+
+print(double(3))
+print(cube(3))
+print(square(5))
+print(avg(3,4,5))
