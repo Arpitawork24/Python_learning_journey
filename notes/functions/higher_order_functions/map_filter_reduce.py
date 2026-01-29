3550310a2ce34c7e6fd47ab27d1a7095bb1860fd
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-29 20:39:53 +0530

    map

--- notes/functions/higher_order_functions/map_filter_reduce.py
+++ notes/functions/higher_order_functions/map_filter_reduce.py
@@ -1,6 +1,8 @@
 # map, filter and reduce are built in func
 # they are higher order functions as they take other func as arguments
 
+# The map function applies a function to each element in a sequence and returns a new sequence containing the transformed elements.
+
 def cube(x):
     return x*x*x
 
@@ -8,7 +10,10 @@ l = [1,2,3,4,5]
 # for val in l:
 #     print (val*val*val, end=" ")
 
-newl = []
-for item in l :
-    newl.append(cube(item))
+# newl = []
+# for item in l :
+#     newl.append(cube(item))
+# print(newl)
+
+newl = list(map(cube,l))
 print(newl)
