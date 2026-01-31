19d2cdad08b71aaf3f7f9c0ed9a03ceaf12968e1
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-01-31 22:59:07 +0530

    oops constructor

--- notes/OOPS/constructors.py
+++ notes/OOPS/constructors.py
@@ -1,4 +1,16 @@
 class Person():
+    def __init__(self, n, o):
+        self.name = n
+        self.occ = o
+    def info(self):
+        print(f"{self.name} is {self.occ}")
     
-    def info():
-        print(f"{self}")
+a = Person("Arpita", "SWE")
+a.info()
+# b= Person() # this will give error as no arg are present in b 
+b = Person("RODO","WEBDEV")
+b.info()
+c = Person(1,2)
+c.info()
+# d = Person(1,2,3)
+# d.info()  # this will give error as only 2 args are possible here.
