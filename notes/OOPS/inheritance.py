aee54ea76dccea7149a3851c84fc9fbbceb27dc7
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-02-05 01:13:32 +0530

    inheritance

---
+++ notes/OOPS/inheritance.py
@@ -0,0 +1,20 @@
+# Inheritance - When a class derives from another class
+
+class Employee: #base class
+  def __init__(self, name, id):
+    self.name = name
+    self.id = id 
+
+  def showDetails(self):
+    print(f"The name of Employee: {self.id} is {self.name}")
+
+class Programmer(Employee):  # derived class or inherited class 
+  def showLanguage(self):
+    print("The default langauge is Python")
+
+
+e1 = Employee("Rohan Das", 400)
+e1.showDetails()
+e2 = Programmer("Harry", 4100)
+e2.showDetails()
+e2.showLanguage()
