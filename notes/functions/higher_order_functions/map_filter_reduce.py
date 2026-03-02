d6f2e5130c15d81249f8789e3a630881af4b686f
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-03-02 20:38:14 +0530

    comments

--- notes/functions/higher_order_functions/map_filter_reduce.py
+++ notes/functions/higher_order_functions/map_filter_reduce.py
@@ -4,21 +4,24 @@
 # MAP
 # The map function applies a function to each element in a sequence and returns a new sequence containing the transformed elements.
 # syntax - map(function, iterable)
+# It returns a map object, so usually we do:
+# list(map(function, iterable))
+
 
 def cube(x):
     return x*x*x
 
-l = [1,2,3,4,5]
+numbers = [1,2,3,4,5]
 
-# for val in l:
+# for val in numbers:
 #     print (val*val*val, end=" ")
 
 # newl = []
-# for item in l :
+# for item in numbers :
 #     newl.append(cube(item))
 # print(newl)
 
-newl = list(map(cube,l))
+newl = list(map(cube, numbers))
 print(newl)
 
 # FILTER
@@ -27,14 +30,15 @@ print(newl)
 
 def fl(x):
     return x>3
-l2 = list(filter(fl,l))
+l2 = list(filter(fl,numbers))
 print(l2)
 
 # REDUCE
 # The reduce function is a higher-order function that applies a function to a sequence and returns a single value. It is a part of the functools module in Python
+# It keeps combining elements
 # syntax - reduce(function, iterable)
 
 from functools import reduce
 
-mysum = reduce(lambda x,y : x+y,l)
+mysum = reduce(lambda x,y : x+y,numbers)
 print(mysum)
