15a0f662767242433fc1d66a7a548ccd31178ce6
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-03-05 11:41:46 +0530

    created basic pdf merger

---
+++ mini_projects/merge_pdf/main.py
@@ -0,0 +1,14 @@
+import pypdf
+
+from pypdf import PdfWriter
+merger = PdfWriter()
+
+print("Library working")
+
+pdfs = ["mini_projects/merge_pdf/file1.pdf", "mini_projects/merge_pdf/file2.pdf", "mini_projects/merge_pdf/file3.pdf"]
+
+for pdf in pdfs :
+    merger.append(pdf)
+
+merger.write("mini_projects/merge_pdf/merged.pdf")
+merger.close()
