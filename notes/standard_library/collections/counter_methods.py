3f46e7b768f9370fbe9263bf4aadf5c446c7d58a
Arpitawork24 <arpitasutariya2005@gmail.com>
2026-03-08 23:22:53 +0530

    counter methods

--- notes/standard_library/collections/counter_methods.py
+++ notes/standard_library/collections/counter_methods.py
@@ -1,4 +1,18 @@
 from collections import Counter
 ctr = Counter([1, 2, 4])
+
+#  update(): Adds counts from another iterable or mapping
 ctr.update([2, 2, 3, 3, 3, 1])
+print(ctr)
+
+# most_common(): Returns a list of the n most common elements and their counts from the most common to the least.
+ctr = Counter([1, 4, 2, 3, 4, 2, 4])
+common = ctr.most_common(2)
+print(common)
+
+#  add(): Increases the count of a single element by 1.
+
+ctr = Counter([1, 2, 2])
+ctr[2] += 1
+ctr[3] += 1
 print(ctr)
